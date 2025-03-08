5728a7be5ff5ed7b7257dde29a32d20ff2fa0346
Gu1lh3rm3-0 <guilherme.oliver2002@gmail.com>
2025-03-08 09:54:21 -0300

    Exercise1 modified

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        String first_name = Console.ReadLine();
+        String firstName = Console.ReadLine();
         Console.Write("What is your second name? ");
-        String second_name = Console.ReadLine();
-        Console.WriteLine($"Your name is {second_name}, {first_name} {second_name} ");
+        String secondName = Console.ReadLine();
+        Console.WriteLine($"Your name is {secondName}, {firstName} {secondName} ");
     }
 }

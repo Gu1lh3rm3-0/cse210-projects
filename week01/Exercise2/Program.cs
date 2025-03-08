39f61b9ce79a99e02679905bfb9b85edddb05e6f
Gu1lh3rm3-0 <guilherme.oliver2002@gmail.com>
2025-03-08 09:54:13 -0300

    Exercise 2 finished

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade? ");
+        String grade = Console.ReadLine();
+        int percentageGrade = int.Parse(grade);
+
+        string letter = "";
+
+        if (percentageGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentageGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentageGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentageGrade >= 60)
+        {
+            letter = "D";
+        }
+        else if (percentageGrade < 60)
+        {
+            letter = "F";
+        }
+        if (letter == "F" || letter == "D")
+        {
+            Console.WriteLine("I'm sorry but you didn't pass, better luck next time!");
+        }
+        else if (letter == "C" || letter == "B" || letter == "A")
+        {
+            Console.WriteLine("Congratulations you passed the course!");
+        }
     }
 }

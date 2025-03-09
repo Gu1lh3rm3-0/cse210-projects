8f6b7d7e8a28c008f2daa100b87be08f6555e150
Gu1lh3rm3-0 <guilherme.oliver2002@gmail.com>
2025-03-08 22:09:15 -0300

    Assignment 3 finished

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,101);
+
+        int userGuess = -1 ;
+
+        int magicNumber = number;
+
+        while (userGuess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > userGuess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < userGuess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed right!");
+            }
+        }
+
     }
 }
